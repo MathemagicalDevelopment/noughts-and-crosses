e520b950be585969b0c3576a46cba9ca39f6a768
MathemagicalDevelopment <development@mathemagicalconsulting.co.uk>
2025-01-15 13:44:05 +0000

    feat: modal, simple component

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { WinnerState } from '../types';
+import { ModalContainer, ModalContent } from './styles';
 
-const Modal = () => {
-    return (
-        <div>Modal</div>
-    )
+type ModalProps = {
+    handleClose: () => void;
+    winner: WinnerState;
 }
+
+const Modal = ({ handleClose, winner }: ModalProps) => <ModalContainer>
+    <ModalContent>
+        <h1>{winner === 'draw' ? 'Draw' : `${winner} wins!`}</h1>
+        <button onClick={handleClose}>Close</button>
+    </ModalContent>
+</ModalContainer>
 
 export default Modal
