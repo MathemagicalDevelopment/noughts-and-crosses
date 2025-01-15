cd73f0f5639a3848b736041f01762263b5873560
MathemagicalDevelopment <development@mathemagicalconsulting.co.uk>
2025-01-15 13:38:39 +0000

    feat: flesh out game set-up page

--- frontend/src/components/SetUp.tsx
+++ frontend/src/components/SetUp.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { ChangeEvent, Suspense, useState } from 'react'
+import { Button, Col, Container, GameTitle, LoadingSpinner, Row, SmallTitle } from './styles'
+import { useNavigate } from 'react-router';
+import createNewGame from '../services/createNewGame';
 
 const SetUp = () => {
+    const [gameId, setGameId] = useState<string | undefined>();
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (gameId: string): void => {
+        if (gameId) navigate(`/online/${gameId}`);
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setGameId(e.target.value);
+    }
+
+    const handleClick = (): void => {
+        if (gameId) {
+            // navigate to game
+            handleNavigation(gameId);
+        } else {
+            // initiate gameId
+            setGameId('');
+        }
+    }
+
+    const createGame = async (): Promise<void> => {
+        const { gameId } = await createNewGame();
+        if (gameId) {
+            handleNavigation(gameId);
+        } else {
+            alert('Error creating game')
+        }
+    }
+
     return (
-        <div>SetUp</div>
+        <Container>
+            <GameTitle>Set-Up</GameTitle>
+            <Row>
+                <Col>
+                    <SmallTitle>Online Play</SmallTitle>
+                    <Suspense fallback={LoadingSpinner}>
+                        <Button key={'game-create'} onClick={createGame}>Create Game</Button>
+                        {typeof gameId === 'undefined' ? <input type="text" placeholder='Enter Game ID' value={gameId} onChange={handleChange} /> :
+                            <Button key={'game-find'} onClick={handleClick}>{typeof gameId === 'undefined' ? `Find Game` : `Join`}</Button>}
+                    </Suspense>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
